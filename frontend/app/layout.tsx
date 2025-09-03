ad949361cdd870070fc6077fa5c55152d11d8901
RUKUNDO BAHATI Sam <rukundorca@gmail.com>
2025-09-03 18:18:25 +0200

    fising favicon issues

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,12 +21,11 @@ export const metadata: Metadata = {
       { url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180' },
-    ],
-    other: [
-      { rel: 'mask-icon', url: '/safari-pinned-tab.svg' },
     ],
   },
   manifest: '/site.webmanifest',
